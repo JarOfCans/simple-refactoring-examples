8a8c6124f4da1d356549e06a498ab74a8a58260c
JarOfCans <johnharbeck3.14@gmail.com>
2021-10-15 22:22:00 -0500

    confused submission
    q1, q2, and print both q2s added. I still have no clue if this is the intended q1

--- src/q1/extract_method/refactored/A.java
+++ src/q1/extract_method/refactored/A.java
@@ -4,35 +4,42 @@ import java.util.List;
 
 public class A {
    Node m1(List<Node> nodes, String p) {
-      // TODO: Your answer
+      extractedMethod(nodes, p);
       // other implementation
       return null;
    }
 
    Edge m2(List<Edge> edgeList, String p) {
-      // TODO: Your answer
+      extractedMethod(edgeList, p);
       // other implementation
       return null;
    }
 
    // TODO: Your answer
-   // ? extractedMethod(?) {
-   //
-   // }
+   public <T extends Graph> void extractedMethod(List<T> inputList, String queryString) {
+	   	//Tried to figure out what I was supposed to do but everything pointed to p5 
+	   	//or a worse version of p5 so.... p5 but reworded it is.
+	   	//In theory, I could have also put the loop in Graph but at that point it's
+	   	//extract superclass rather than extract method
+	   	//There also was p1, but that would just be replacing an output
+	   	for (T check: inputList) {
+	   		if (check.contains(queryString)) {
+	   			System.out.println(check);
+	   		}
+	   	}
+   	}
 }
 
-class Node {
-   String name;
+class Graph {
+	String name;
 
-   public boolean contains(String p) {
-      return name.contains(p);
-   }
+	public boolean contains(String p) {
+	    return name.contains(p);
+	}
 }
 
-class Edge {
-   String name;
+class Node extends Graph {
+}
 
-   public boolean contains(String p) {
-       return name.contains(p);
-   }
+class Edge extends Graph {
 }
---
+++ src/q2/outputboth/PrintQ2.java
@@ -0,0 +1,9 @@
+package q2.outputboth;
+
+public class PrintQ2 {
+	public static void main(String args[]) {
+		System.out.println("Q2:");
+		q2.form_template_method1.org.A.main(args);
+		q2.form_template_method1.refactored.A.main(args);
+	}
+}
